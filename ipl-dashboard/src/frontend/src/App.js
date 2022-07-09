7ae49487d8b3bfd97e3bb93c852b522d8b339c06
Shivam Agrawal <shivam.agrawal1501@gmail.com>
2022-07-09 18:26:58 +0530

    Match API Created backend

--- ipl-dashboard/src/frontend/src/App.js
+++ ipl-dashboard/src/frontend/src/App.js
@@ -1,7 +1,8 @@
 
 import './App.css';
-import {BrowserRouter as Router,Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router,Route, Routes, Switch} from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
+import { MatchPage } from './pages/MatchPage';
 
 function App() {
   return (
--- ipl-dashboard/src/frontend/src/pages/MatchPage.js
+++ ipl-dashboard/src/frontend/src/pages/MatchPage.js
@@ -3,15 +3,28 @@ import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 import { BrowserRouter as Router,Route, Routes,useParams } from 'react-router-dom';
 
-export const TeamPage=()=> {
-
-
+export const MatchPage=()=> {
+  const [matches, setMatches] = useState([]);
+  const { teamName, year } = useParams();
+  useEffect(
+    ()=>{
+        const fetchMatches= async()=>{
+            const response=await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+            const data=await response.json();
+            setMatches(data);
+            console.log(data);
+        };
+        fetchMatches();
+    },[teamName,year]
+    
+    );
   return (
-    <div className="TeamPage">
-      <h1>{team.teamName}</h1>
-      <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
-      {team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName} key={match.id}  match={match} />)}
+    <div className="MatchPage">
       
+      <h1 className="page-heading">{teamName} matches in {year}</h1>
+                {
+                    matches.map(match => <MatchDetailCard key={match.id} teamName={teamName} match={match} />)
+                }
       
     </div>
   );
