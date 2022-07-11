8664dce239183ec8e5de5670ed06a954458be42c
Shivam Agrawal <shivam.agrawal1501@gmail.com>
2022-07-11 12:53:27 +0530

    added homepage

--- ipl-dashboard/src/frontend/src/App.js
+++ ipl-dashboard/src/frontend/src/App.js
@@ -1,23 +1,25 @@
-
 import './App.scss';
-import {BrowserRouter as Router,Route, Routes, Switch} from 'react-router-dom';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
 import { MatchPage } from './pages/MatchPage';
+import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes >
         <Switch>
-        <Route path="/teams/:teamName/matches/:year">
+          <Route path="/teams/:teamName/matches/:year">
             <MatchPage />
           </Route>
           <Route path="/teams/:teamName">
             <TeamPage />
           </Route>
-          </Switch>
-        </Routes>
+          <Route path="/">
+            <HomePage />
+          </Route>
+
+        </Switch>
       </Router>
     </div>
   );
